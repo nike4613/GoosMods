7c025fdda395f9d794b3780f1ce6346a0ae81885
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-01-30 20:07:28 -0600

    Add project files.

---
+++ FastGoos/FixHarmony.cs
@@ -0,0 +1,36 @@
+﻿using Harmony;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FastGoos
+{
+    [HarmonyPatch(typeof(CodeTranspiler), nameof(CodeTranspiler.ConvertInstructionsAndUnassignedValues))]
+    internal class FixHarmony
+    {
+        public static bool Prefix(Type type, IEnumerable enumerable, ref Dictionary<object, Dictionary<string, object>> unassignedValues,
+            ref IEnumerable __result)
+        {
+			var enumerableAssembly = type.GetGenericTypeDefinition().Assembly;
+			var genericListType = enumerableAssembly.GetType(typeof(List<>).FullName);
+			var elementType = type.GetGenericArguments()[0];
+			var listType = /*enumerableAssembly.GetType(*/genericListType.MakeGenericType(new Type[] { elementType })/*.FullName)*/;
+			var list = Activator.CreateInstance(listType);
+			var listAdd = list.GetType().GetMethod("Add");
+			unassignedValues = new Dictionary<object, Dictionary<string, object>>();
+			foreach (var op in enumerable)
+			{
+				var elementTo = CodeTranspiler.ConvertInstruction(elementType, op, out var unassigned);
+				unassignedValues.Add(elementTo, unassigned);
+				listAdd.Invoke(list, new object[] { elementTo });
+				// cannot yield return 'elementTo' here because we have an out parameter in the method
+			}
+			__result = list as IEnumerable;
+
+			return false;
+        }
+    }
+}
---
+++ FastGoos/GoosMod.cs
@@ -0,0 +1,47 @@
+﻿using Harmony;
+using LoadBins;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using GooseDesktop;
+using System.Reflection.Emit;
+using SamEngine;
+
+namespace FastGoos
+{
+    [Plugin]
+    public class GoosMod
+    {
+        internal static HarmonyInstance harmony;
+        internal static Assembly goosAssembly;
+
+        [Plugin]
+        public static void Init(List<MethodInfo> entries)
+        {
+            HarmonyInstance.DEBUG = true;
+            harmony = HarmonyInstance.Create("FastGoos");
+            harmony.PatchAll();
+
+            goosAssembly = entries.First().DeclaringType.Assembly;
+
+            var goosTick = goosAssembly.GetType("GooseDesktop.TheGoose").GetMethod("Tick");
+
+            var transpiler = new HarmonyMethod(typeof(GoosMod).GetMethod(nameof(GoosTickTranspiler)));
+            harmony.Patch(goosTick, transpiler: transpiler);
+        }
+
+        public static IEnumerable<CodeInstruction> GoosTickTranspiler(IEnumerable<CodeInstruction> il_)
+        {
+            var il = new List<CodeInstruction>(il_);
+            foreach (var inst in il)
+            {
+                if (inst.opcode == OpCodes.Ldc_R4 && (float)inst.operand == Time.deltaTime)
+                    inst.operand = Time.deltaTime * 5;
+            }
+            return il;
+        }
+    }
+}
