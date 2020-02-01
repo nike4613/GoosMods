47347d526baabe805f15dc117f4faa6a57e3cdec
Anairkoen Schno <aaron.cirr.com@gmail.com>
2020-01-31 18:20:30 -0600

    Completed HatGoos

--- HatGoos/GoosMod.cs
+++ HatGoos/GoosMod.cs
@@ -3,6 +3,8 @@ using Harmony;
 using LoadBins;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -14,9 +16,17 @@ namespace HatGoos
     public static class GoosMod
     {
         internal static HarmonyInstance harmony;
-        internal static Assembly goosAssembly;
 
         internal const string Name = "HatGoos";
+        
+        public enum HatType
+        { Default, None, Custom }
+        public static HatType HatMode = HatType.Default;
+        public static float HorizontalSize = 1.5f;
+        public static float HatPosition = .6f;
+        public static string CustomHatPath = "";
+
+        internal static Bitmap hatImage = null;
 
         [Plugin]
         public static void Init(List<MethodInfo> entries)
@@ -24,35 +34,55 @@ namespace HatGoos
             HarmonyInstance.DEBUG = true;
             harmony = HarmonyInstance.Create(Name);
             harmony.PatchAll();
+            GoosRenderPatch.Apply(harmony);
 
             GoosConfig.TryRead += TryReadConfigValue;
             GoosConfig.Stringify += StringifyConfigOptions;
             GoosConfig.Init += InInit;
-
-            goosAssembly = entries.First().DeclaringType.Assembly;
         }
 
         public static bool TryReadConfigValue(KeyValuePair<string, string> pair)
         {
             switch (pair.Key)
             {
-                /*case Name + "." + nameof(TimeMultiplier):
-                    TimeMultiplier = Convert.ToSingle(pair.Value);
-                    return true;
-                case Name + "." + nameof(RealTimeMultiplier):
-                    RealTimeMultiplier = Convert.ToSingle(pair.Value);
-                    return true;*/
+                case Name + "." + nameof(HatMode):
+                    return Enum.TryParse(pair.Value, out HatMode);
+                case Name + "." + nameof(HorizontalSize):
+                    return float.TryParse(pair.Value, out HorizontalSize);
+                case Name + "." + nameof(HatPosition):
+                    return float.TryParse(pair.Value, out HatPosition);
+                case Name + "." + nameof(CustomHatPath):
+                    if ((HatMode == HatType.Custom && File.Exists(pair.Value)) || HatMode != HatType.Custom)
+                    {
+                        CustomHatPath = pair.Value;
+                        return true;
+                    }
+                    else return false;
                 default: return false;
             }
         }
 
-        public static string StringifyConfigOptions() => ""
-            /*$"{Name}.{nameof(TimeMultiplier)}={Convert.ToString(TimeMultiplier)}\n" +
-            $"{Name}.{nameof(RealTimeMultiplier)}={Convert.ToString(RealTimeMultiplier)}\n"*/;
+        public static string StringifyConfigOptions() =>
+            $"{Name}.{nameof(HatMode)}={Convert.ToString(HatMode)}\n" +
+            $"{Name}.{nameof(HorizontalSize)}={Convert.ToString(HorizontalSize)}\n" +
+            $"{Name}.{nameof(HatPosition)}={Convert.ToString(HatPosition)}\n" +
+            $"{Name}.{nameof(CustomHatPath)}={CustomHatPath}\n";
 
         internal static void InInit()
         {
-
+            switch (HatMode)
+            {
+                case HatType.None:
+                    hatImage = new Bitmap(1, 1);
+                    hatImage.SetPixel(0, 0, Color.Transparent);
+                    break;
+                case HatType.Default:
+                    hatImage = Resources.Default;
+                    break;
+                case HatType.Custom:
+                    hatImage = new Bitmap(CustomHatPath);
+                    break;
+            }
         }
     }
 }
---
+++ HatGoos/GoosRenderPatch.cs
@@ -0,0 +1,95 @@
+﻿using Harmony;
+using SamEngine;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HatGoos
+{
+    internal class GoosRenderPatch
+    {
+        private static Func<Vector2> GetNeckHeadPoint;
+        private static Func<float> GetDirection;
+        private static int HeadRadius;
+
+        public static void Apply(HarmonyInstance inst)
+        {
+            var theGoos = typeof(Time).Assembly.GetType("GooseDesktop.TheGoose");
+            var theGoosRig = theGoos.GetNestedType("Rig", BindingFlags.NonPublic);
+            HeadRadius = (int)theGoosRig.GetField("HeadRadius1", BindingFlags.Public | BindingFlags.Static).GetValue(null);
+
+            var goosRigField = theGoos.GetField("gooseRig", BindingFlags.NonPublic | BindingFlags.Static);
+            var goosDirectionField = theGoos.GetField("direction", BindingFlags.NonPublic | BindingFlags.Static);
+            var rigHeadPointField = theGoosRig.GetField("neckHeadPoint", BindingFlags.Public | BindingFlags.Instance);
+
+            GetNeckHeadPoint = GenerateGetter<Vector2>(goosRigField, rigHeadPointField);
+            GetDirection = GenerateGetter<float>(goosDirectionField);
+
+            var render = theGoos.GetMethod("Render", BindingFlags.Public | BindingFlags.Static);
+
+            var patch = new HarmonyMethod(typeof(GoosRenderPatch), nameof(Render));
+            inst.Patch(render, postfix: patch);
+        }
+
+        private static Func<T> GenerateGetter<T>(FieldInfo first, params FieldInfo[] fields)
+        {
+            var dynMethod = new DynamicMethod($"get {string.Join(".", fields.Prepend(first).Select(f => f.Name))}", typeof(T), Type.EmptyTypes, true);
+            var il = dynMethod.GetILGenerator();
+            il.Emit(OpCodes.Ldsfld, first);
+            foreach (var fld in fields)
+                il.Emit(OpCodes.Ldfld, fld);
+            il.Emit(OpCodes.Ret);
+
+            return (Func<T>)dynMethod.CreateDelegate(typeof(Func<T>));
+        }
+
+        public static void Render(Graphics g)
+        {
+            if (GoosMod.hatImage == null) return;
+
+            var direction = GetDirection() + 90f; ;
+            var headPoint = GetNeckHeadPoint();
+
+            var vertBase = (HeadRadius / 2) * GoosMod.HatPosition;
+
+            var bmp = GoosMod.hatImage;
+            var baseOffset = HeadRadius * GoosMod.HorizontalSize / 2;
+            var vertOffset = (bmp.Height / bmp.Width) * baseOffset * 2;
+
+            var set = new[] {
+                new Vector2(-baseOffset, vertBase + vertOffset),
+                new Vector2(baseOffset, vertBase + vertOffset),
+                new Vector2(-baseOffset, vertBase)
+            };
+
+            float? sin = null, cos = null;
+            var rotated = set.Select(v => Rotate(v, direction, ref sin, ref cos));
+            var translated = rotated.Select(v => v + headPoint);
+            var asPoints = translated.Select(ToPoint).ToArray();
+
+            // define a parallelogram for it
+            g.DrawImage(GoosMod.hatImage, asPoints);
+        }
+
+        private static Vector2 Rotate(Vector2 point, float degrees, ref float? sin, ref float? cos)
+        {
+            if (sin == null) sin = Sin(degrees);
+            if (cos == null) cos = Cos(degrees);
+            return new Vector2(point.x * cos.Value - point.y * sin.Value,
+                               point.y * cos.Value + point.x * sin.Value);
+        }
+
+        private static float Sin(float deg)
+            => (float)Math.Sin(deg * (Math.PI / 180d));
+        private static float Cos(float deg)
+            => (float)Math.Cos(deg * (Math.PI / 180d));
+
+        private static Point ToPoint(Vector2 vector)
+            => new Point((int)vector.x, (int)vector.y);
+    }
+}
